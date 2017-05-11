20e7c4f2b8f2e558190e36d38e387ee0ff9c1125
Kevin Paul <kpaul@ucar.edu>
2017-05-11 11:49:33 -0600

    Adding strace-friendly exec script for running tests

---
+++ sbin/exec-run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ $STRACE -ne 0 ]; then
+	BASENAME=$(basename $PWD | sed -- "s/-/./")
+	TIMESTAMP=$(date +%Y%m%d%H%M%S)	
+	STRACELOG=$BASENAME.$RUNNAME.$HOSTNAME.$TIMESTAMP.$$.strace
+	CMD="strace -tt -T -o $STRACELOG $@"
+else
+	CMD="$@"
+fi
+
+exec $CMD
