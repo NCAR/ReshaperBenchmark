ea6a7ac011cdf261e4acc474a7411e2b7b162163
Kevin Paul <kpaul@ucar.edu>
2017-09-01 15:05:49 -0600

    Simplify required test arguments

--- tests/small/arguments.sh
+++ tests/small/arguments.sh
@@ -1,5 +1,2 @@
 export GENARGS="-d 10,100 -v 20,0,1 -n 12"
-export GENLOG="gendata.$TESTNAME.$RUNNAME.$HOSTNAME.$(date +%Y%m%d%H%M%S).log"
-export RUNARGS="--testname $TESTNAME --runname $RUNNAME --hostname $HOSTNAME"
-export RUNLOG="reshaper.$TESTNAME.$RUNNAME.$HOSTNAME.$(date +%Y%m%d%H%M%S).log"
     
